74b23f80c636bbc8e13cc7ea5dc194a2864422a8
Nikunj <khokharnikunj8@gmail.com>
2017-12-13 13:45:15 +0530

    Add ScrollPosition

---
+++ ScrollPosition/app/src/main/java/com/practice/scrollposition/MainActivity.java
@@ -0,0 +1,21 @@
+package com.practice.scrollposition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter<CharSequence> arrayAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("main","YES");
+        listView=(ListView)findViewById(R.id.listView);
+        arrayAdapter=ArrayAdapter.createFromResource(   this,R.array.all_items,android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+    }
+}
